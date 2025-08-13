9940231b3c54f8fa954dece97bbaf554a74a102f
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-08-14 00:52:55 +0530

    Add introductory notes on machine learning and its types

---
+++ 1_Intro.py
@@ -0,0 +1,27 @@
+# Supervised Learning
+# Maps an Input X to Output Label Y
+# Learns from being given "right answers"
+
+# Types Regression ,classification
+# Regression : Predict any number out of infinitely many possible outcomes
+
+# Classification : Predict out of limited data (predict categories not necessarily numbers)
+# Small number of possible outcomes
+
+
+# Unsupervised Learning
+# Data comes with only Input X And no output labels Y
+# we are given data that is not associated with any output data that is label Y
+# No one is there to supervise the data, Data  learns on its own , patterns ,structures that might be in the data
+
+
+# Clustering Algorithm
+# When Algorithm is able to place the unlabeled Data into differnet clusters
+# Group similar data points togther
+
+# Anomaly Detection
+# Find unusual data points
+
+# Dimensionality Reduction
+# Take a big data set and let us magically compress it
+# Compress data using fewer numbers losing as little info as possible
