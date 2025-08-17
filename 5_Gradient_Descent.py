e01af197fc2655f86c2f63f4547258202b7180d5
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-08-18 00:30:48 +0530

    Gradient Descent Videos

---
+++ 5_Gradient_Descent.py
@@ -0,0 +1,5 @@
+# Gradient Descent Algorithm is used to find out the best w and b to find out minimum cost function as with higher data values it is difficult to find out values of w and b manually
+
+# Not just minimum cost function for linear regression for any function
+
+ 
