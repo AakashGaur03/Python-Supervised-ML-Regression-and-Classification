4e6e8d43ac38a4a41680f45c7848078129aa2a39
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-08-23 00:15:49 +0530

    Updated Folder Structure and Feature Scaling

---
+++ Week2/Lab2_Multiple_Variable_Sol/Lab3_Feature_Scaling_Learning_Rate/feature_scaling.py
@@ -0,0 +1,21 @@
+# We rescale the data as in larger data values it is slow to calculate gradient descent
+
+# so For Example
+
+
+# -1     <= x1 <=   1         // No need to Scale
+# -3     <= x2 <=   3         // No need to Scale
+# -0.3   <= x3 <=   0.3       // No need to Scale
+# 0      <= x4 <=   3         // No need to Scale
+# -2     <= x5 <=   0.5       // No need to Scale
+# -100   <= x6 <=   100       // Too large rescale
+# -0.001 <= x7 <=   0.001     // Too Small rescale
+# 98     <= x8 <=   105       // Too large rescale
+
+# when in doubt that to do feature scaling or not just do it
+# As there is not any such harm to carry feature re-scaling
+
+
+# There are different Techniques to do so 
+# For example Divide each value by the maximim value for the feature 
+# we will see in depth in Future Lab
