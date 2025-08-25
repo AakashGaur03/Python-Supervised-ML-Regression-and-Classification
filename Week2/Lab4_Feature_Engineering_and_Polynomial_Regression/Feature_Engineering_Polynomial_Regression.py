7bfec0476b2a893a306f42eb5bf95ed89492836f
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-08-26 00:55:42 +0530

    Rewatched Feature Scaling and Learning Rate and Feature Engineering Polynomial Regression

---
+++ Week2/Lab4_Feature_Engineering_and_Polynomial_Regression/Feature_Engineering_Polynomial_Regression.py
@@ -0,0 +1,9 @@
+# Feature Engineering is in which we use our knowledge or intution about the porblem tod design new features usually by transforming or combining orginal Features making easier for learning algorithm to make accurate predictions
+
+
+# Polynomial Regression
+# we can do this using 
+# square or cubic functions
+# or
+# using square roots
+# we will discuss in depth
